aa8745a65094cc4e6125357ec96a67f750637490
Addder123 <99239379+Addder123@users.noreply.github.com>
2023-03-29 17:20:42 -0500

    Update NAME_FUNC.cpp

--- NAME_FUNC.cpp
+++ NAME_FUNC.cpp
@@ -3,23 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-void genShowMatrix(int visible[][10]) {
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
-            visible[i][j] = -1;
-        }
-    }
-}
-
-void genHideMatrix(int hidden[][10], int lower, int upper) {
-    srand(time(NULL));
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
-            hidden[i][j] = rand() % (upper - lower + 1) + lower;
-        }
-    }
-}
-
 std::string GET_NAME(std::string USER_NAME)
 {
     int ALPHA = 1;
@@ -66,33 +49,3 @@ std::cout << "Welcome: " << USER_NAME << "\n";
 return USER_NAME;
 }
 
-
-int main()
-{
-    std::string USER_NAME;
-    int visible[10][10];
-    int hidden[10][10];
-    genShowMatrix(visible);
-    genHideMatrix(hidden, 1, 10);
-    
-    USER_NAME = GET_NAME(USER_NAME);
-    
-    // Print the contents of the matrices for testing purposes
-    std::cout << "Visible matrix:\n";
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
-            std::cout << visible[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-
-   /* std::cout << "Hidden matrix:\n";
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
-            std::cout << hidden[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-    */
-    return 0;
-}
