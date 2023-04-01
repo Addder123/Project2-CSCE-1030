70f098aa74ec9824b7d28b0e136975307ab3cf51
Addder123 <99239379+Addder123@users.noreply.github.com>
2023-04-01 14:08:07 -0500

    Update DISPLAY_LEFT.cpp

--- DISPLAY_LEFT.cpp
+++ DISPLAY_LEFT.cpp
@@ -2,14 +2,10 @@
 
 int DISPLAY_LEFT(int matrix[][COL], int DISPLAY_LEFT , int LOWER_BOUND)//will need to call hidden matrix
 {
-    DISPLAY_LEFT = LOWER_BOUND;
-    for(int i = 0; i < ROW ; ++i ){
-        for(int j = 0 ; j < COL ; ++j){
-            if(LOWER_BOUND > matrix[i][j]){
-                LOWER_BOUND = matrix[i][j];
-            }
-        }
-    }
-    DISPLAY_LEFT = LOWER_BOUND;
+    if(DISPLAY_LEFT == -1){
+   DISPLAY_LEFT = LOWER_BOUND; 
+  }
+    else{
+        std::cout << "You can't do that cheater!\n";
     return DISPLAY_LEFT;
 }
