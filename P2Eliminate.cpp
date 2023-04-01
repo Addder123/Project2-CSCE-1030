b492cc024c7ef8c7ca09ba78434af6f88a9a496e
joelrhunt <42118430+joelrhunt@users.noreply.github.com>
2023-04-01 15:01:58 -0500

    Add files via upload

---
+++ P2Eliminate.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+void eliminate(array[][], hidden_array[][], int rowIn, int colIn)
+{
+	bool check = false;
+
+	if(!(check = false))
+	{
+		for(int i = 0; i < ROWS; ++i)
+		{
+			for(int j = 0; j < COLS; ++j)
+			{
+
+				array[i][colIn] = 0;
+				array[rowIn][j];
+				hidden_array[rowIn][j] = 0;
+				hidden_array[i][colIn = 0;
+			}
+		}
+	}
+	cout << endl;
+}
